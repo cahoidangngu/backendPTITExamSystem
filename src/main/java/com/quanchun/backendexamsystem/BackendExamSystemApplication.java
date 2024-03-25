122f6126576a78ff78dec3e2322f580e3a336a1c
cahoidangngu <nhinhong.1402@gmail.com>
2024-03-26 01:56:21 +0700

    initialize

---
+++ src/main/java/com/quanchun/backendexamsystem/BackendExamSystemApplication.java
@@ -0,0 +1,13 @@
+package com.quanchun.backendexamsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendExamSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendExamSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quanchun/backendexamsystem/BackendExamSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanchun.backendexamsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendExamSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
